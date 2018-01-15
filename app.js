58cdb864661d7c5bb592504b08e450e4d2180a63
Jessica Bohn <bohn.16@wright.edu>
2018-01-15 05:23:12 -0600

    Add first route
    - Add first route for '/expressions'
    - Add get request for route
    - Add static public server

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+app.get('/expressions', (req, res, next) => {
+  console.log(`${req}`);
+});
 
 // Invoke the app's `.listen()` method below:
 app.listen(PORT, () => {
